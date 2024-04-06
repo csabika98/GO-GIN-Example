adeb4df94f287920e4fb8e3f393d4233eef74bf5
csabika98 <6ix9inegotti@naver.com>
2024-04-06 07:00:13 +0200

    serving HTML pages with GO GIN

---
+++ html_serve.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Static("/static", "./static")
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	r.Run(":8080")
+}
